6ea92248c8f61dc632038ae5bb7f982978fbfc41
Hopp-Murithi <hoppmurithi@gmail.com>
2023-06-14 15:14:50 +0300

    chore: set up class as a service architcture

--- wp-content/plugins/easy-manage/easy-manage.php
+++ wp-content/plugins/easy-manage/easy-manage.php
@@ -5,7 +5,7 @@
  */
 
 /**
- *Plugin Name: employees plugin
+ *Plugin Name: EasyManage plugin
  Plugin URI: http://...	
  Description: This is a plugin built to help mange different users of a training management system
  version: 1.0.0
@@ -26,6 +26,7 @@ if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
 
 
 use Inc\Base;
+use  Inc\Init;
 
 //activate plugin
 function activate_easymanage_plugin()
@@ -42,3 +43,9 @@ function deactivate_easymanage_plugin()
 register_activation_hook(__FILE__, 'activate_easymanage_plugin');
 
 register_deactivation_hook(__FILE__, 'deactivate_easymanage_plugin');
+
+// to add the plugin services
+
+if (class_exists('Inc\\Init')) {
+    Init::register_services();
+}
---
+++ wp-content/plugins/easy-manage/inc/Init.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * 
+ * @package easymanage
+ */
+
+namespace Inc;
+
+class Init
+{
+    public static function get_services()
+    {
+        return [
+            Pages\PMroutes::class,
+           
+        ];
+    }
+
+    public static function register_services()
+    {
+        foreach (self::get_services() as $class) {
+            $service = self::instantiate($class);
+            if (method_exists($service, 'register')) {
+                $service->register();
+            }
+        }
+    }
+
+    private static function instantiate($class)
+    {
+        $service = new $class;
+        return $service;
+    }
+}
