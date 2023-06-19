d8e342a02fabb21db205d8e4d3ec2c39ea90a27f
Hopp-Murithi <hoppmurithi@gmail.com>
2023-06-19 16:32:12 +0300

    build: enable debug mode

--- wp-config.php
+++ wp-config.php
@@ -79,7 +79,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 define('JWT_AUTH_SECRET_KEY', 'abcdefghij');
 
 /* Add any custom values between this line and the "stop editing" line. */
