a6db66e8b24ab32f4d4821983acc7978cd7f1ad9
Hopp-Murithi <hoppmurithi@gmail.com>
2023-06-22 10:24:07 +0300

    fix: update db fields for project tables

--- wp-content/plugins/easy-manage/inc/Pages/createTables.php
+++ wp-content/plugins/easy-manage/inc/Pages/createTables.php
@@ -1,54 +1,54 @@
 <?php
-
 /**
  * @package easyManage
  */
 
 namespace Inc\Pages;
 
-use WP_Error;
-
 class createTables
 {
-    function register()
+    public function register()
     {
         $this->create_cohort_table();
-        $this->create_projects_table();
+        $this->create_project_table();
     }
 
-    // Create products table
+    // Create cohorts table
     public function create_cohort_table()
     {
         global $wpdb;
         $table_name = $wpdb->prefix . 'cohorts';
         $charset_collate = $wpdb->get_charset_collate();
         $sql = "CREATE TABLE $table_name (
-            id int(9) NOT NULL AUTO_INCREMENT,
-            programme_name varchar(255) NOT NULL,
-            startdate DATE NOT NULL,
-            endDate DATE NOT NULL,
-            assigned_to varchar(255) NOT NULL
+            id int NOT NULL AUTO_INCREMENT,
+            programme_name text NOT NULL,
+            starts_date DATE NOT NULL,
+            end_date DATE NOT NULL,
+            assigned_to text NOT NULL,
+            place text NOT NULL,
             PRIMARY KEY (id)
         ) $charset_collate;";
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
-        
     }
-    public function create_projects_table()
+
+    // Create projects table
+    public function create_project_table()
     {
         global $wpdb;
-        $table_name = $wpdb->prefix . 'cohorts';
+        $table_name = $wpdb->prefix . 'projects';
         $charset_collate = $wpdb->get_charset_collate();
         $sql = "CREATE TABLE $table_name (
-            id int(9) NOT NULL AUTO_INCREMENT,
-            programme_name varchar(255) NOT NULL,
-            startdate DATE NOT NULL,
-            endDate DATE NOT NULL,
-            assigned_to varchar(255) NOT NULL
+            id int NOT NULL AUTO_INCREMENT,
+            project_name text NOT NULL,
+            project_details text NOT NULL,
+            assigned_to text NOT NULL,
+            stack text NOT NULL,
+            due_date DATE NOT NULL,
+            is_completed int NOT NULL default 0,
             PRIMARY KEY (id)
         ) $charset_collate;";
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
-        
     }
 }
